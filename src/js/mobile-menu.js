12c90f2807abb9531a1cf2bf904b36594caaec28
Evhen Polyakov <xxx.qqq1990@gmail.com>
2025-11-08 08:24:11 +0100

    fixed js files

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,21 @@
 const openBtn = document.querySelector('[data-open-mobile-menu]');
 const closeBtn = document.querySelector('.close-mobile-btn');
 const menu = document.querySelector('.mobile-menu');
+const menuLinks = document.querySelectorAll('.mobile-menu .header-nav-link');
 
 openBtn.addEventListener('click', () => {
   menu.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
 });
 
 closeBtn.addEventListener('click', () => {
   menu.classList.remove('is-open');
+  document.body.style.overflow = '';
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+    document.body.style.overflow = '';
+  });
 });
